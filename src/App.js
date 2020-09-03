c6c4345ba41d518efcd248c285ddf6aeaa39d3d7
Matt <mattlaws1982@gmail.com>
2020-09-03 20:22:21 +0100

    First commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+import Row from './Row';
+import requests from './requests';
+
+function App() {
+  return (
+    <div className="app">
+      <Row title="NETFLIX ORIGINALS" 
+      fetchUrl={requests.fetchNetflixOriginals} 
+      isLargeRow
+      />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rate" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romace Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentries" fetchUrl={requests.fetchDocumentaries} />
+    </div>
+  );
+}
+
+export default App;
