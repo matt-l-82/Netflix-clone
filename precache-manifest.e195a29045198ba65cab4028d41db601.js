574e97aab688b12d9f4aab547789558636c9c2b7
Matt <mattlaws1982@gmail.com>
2020-11-19 10:24:04 +0000

    Updates

---
+++ precache-manifest.e195a29045198ba65cab4028d41db601.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62eaa69be182c9ca449fd5cb42212a2a",
+    "url": "/netflix/index.html"
+  },
+  {
+    "revision": "11ee07e5d0e335e8ef78",
+    "url": "/netflix/static/css/main.f4e6126d.chunk.css"
+  },
+  {
+    "revision": "2e2496893e2930f852e6",
+    "url": "/netflix/static/js/2.56b7ef40.chunk.js"
+  },
+  {
+    "revision": "894f61387b383e6e2e0543fb120b4656",
+    "url": "/netflix/static/js/2.56b7ef40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11ee07e5d0e335e8ef78",
+    "url": "/netflix/static/js/main.cb03df28.chunk.js"
+  },
+  {
+    "revision": "0587df36741fdadb7899",
+    "url": "/netflix/static/js/runtime-main.5311aa9e.js"
+  }
+]);
